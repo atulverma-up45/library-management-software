abc8cae529ab77b254a7f43270442ee0ceb70501
mohd kaif <mk2818356@gmail.com>
2025-07-28 17:43:57 +0530

    chagne home page of nextjs

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -16,7 +16,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { useRouter } from "next/navigation";
